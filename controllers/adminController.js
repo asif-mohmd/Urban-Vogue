1277541e207f065d10c394fca1f343709f9226e9
Asif Muhammed <asifasifpsps@gmail.com>
2023-09-09 11:49:53 +0530

    Add product page linked

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -5,29 +5,29 @@ const productModel = require("../models/Product")
 let adminEmail = "admin@gmail.com"
 let adminPassword = "123"
 
-const adminLoginCheck = (req,res,next) =>{
-    if(req.session.admin){
+const adminLoginCheck = (req, res, next) => {
+    if (req.session.admin) {
         console.log("Successssss")
-       next()
-    }else{
+        next()
+    } else {
         console.log("kkkkkkkkkkk")
         res.redirect("/admin/adminLoginView")
     }
-    
+
 }
 
-const adminLoginView = (req,res) =>{
-        res.render("admin/login")
-    }
+const adminLoginView = (req, res) => {
+    res.render("admin/login")
+}
 
-const adminLogin = (req,res) => {
-    const {email , password} = req.body
+const adminLogin = (req, res) => {
+    const { email, password } = req.body
     // console.log(email,password)
-    if(email===adminEmail&&password==adminPassword){
+    if (email === adminEmail && password == adminPassword) {
         req.session.admin = true
         console.log(req.session);
         res.redirect("/admin")
-    }else{
+    } else {
         res.redirect("/admin/adminLoginView")
     }
 }
@@ -45,24 +45,24 @@ const addProductView = async (req, res) => {
 }
 
 const addProduct = async (req, res) => {
-    const {name, price , description , category , size } = req.body
-      
+    const { name, price, description, category, size } = req.body
+
     data = {
         "name": name,
         "price": price,
-        "description": description, 
+        "description": description,
         "category": category,
         "size": size
     }
-    
+
 
     // console.log(name, price , description , category , size )
     const product = await productModel.create(data)
 
-    if(product){
+    if (product) {
         console.log("Product added succesfully")
         res.redirect("/admin")
-    }else{
+    } else {
         console.log("Product not added")
         res.render("admin/add-product")
 
